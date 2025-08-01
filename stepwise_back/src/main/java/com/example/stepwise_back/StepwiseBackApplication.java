b4f7699b1aefd8d3ae474a609094b735dd61ce91
programofktw <thkim000317@naver.com>
2025-08-01 15:05:32 +0900

    [SETTING] : 백엔드 프로젝트 추가

---
+++ stepwise_back/src/main/java/com/example/stepwise_back/StepwiseBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.stepwise_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StepwiseBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StepwiseBackApplication.class, args);
+	}
+
+}
---
+++ stepwise_back/src/test/java/com/example/stepwise_back/StepwiseBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stepwise_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StepwiseBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
