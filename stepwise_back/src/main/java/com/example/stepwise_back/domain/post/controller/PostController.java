1337f7c2348f5fe4ac3a8bf4ea0d5e9115c3adc1
programofktw <thkim000317@naver.com>
2025-08-01 15:19:49 +0900

    [FEAT] : 익명 게시글 관련 게시판 제작

---
+++ stepwise_back/src/main/java/com/example/stepwise_back/domain/post/controller/PostController.java
@@ -0,0 +1,10 @@
+package com.example.stepwise_back.domain.post.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/post")
+public class PostController {
+
+
+
+}
