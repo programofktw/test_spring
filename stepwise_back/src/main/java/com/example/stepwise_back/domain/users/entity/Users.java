2a8ef7280467ce0e52ac18a794eb74d6316475f0
programofktw <thkim000317@naver.com>
2025-08-07 16:13:27 +0900

    issue8/user_class_feat

--- stepwise_back/src/main/java/com/example/stepwise_back/domain/users/entity/Users.java
+++ stepwise_back/src/main/java/com/example/stepwise_back/domain/users/entity/Users.java
@@ -1,10 +1,12 @@
 package com.example.stepwise_back.domain.users.entity;
 
 import com.example.stepwise_back.domain.base.BaseEntity;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import com.example.stepwise_back.domain.post.entity.Post;
+import jakarta.persistence.*;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA용 기본 생성자
@@ -23,12 +25,27 @@ public class Users extends BaseEntity {
     @Column(name = "nick_name", nullable = false)
     private String nickName;
 
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Post> posts = new ArrayList<>();
+
+
     public void updateNickName(String nickName){
         this.nickName = nickName;
     }
 
     public void updatePassword(String password){
         this.password = password;
+    }
+
+
+    public void addPost(Post post) {
+        posts.add(post);
+        post.setUser(this);
+    }
+
+    public void removePost(Post post) {
+        posts.remove(post);
+        post.setUser(null);
     }
 
 }
