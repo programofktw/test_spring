d5dddbf1d6890443b22f6ba157a41f5c81e2464e
programofktw <thkim000317@naver.com>
2025-08-07 17:26:25 +0900

    [FEAT] UserService 구현체 제작

---
+++ stepwise_back/src/main/java/com/example/stepwise_back/domain/users/service/UserServiceImpl.java
@@ -0,0 +1,44 @@
+package com.example.stepwise_back.domain.users.service;
+
+import com.example.stepwise_back.domain.users.service.dto.input.UserDeleteInput;
+import com.example.stepwise_back.domain.users.service.dto.input.UserLoginInput;
+import com.example.stepwise_back.domain.users.service.dto.input.UserNicknameUpdateInput;
+import com.example.stepwise_back.domain.users.service.dto.input.UserRegisterInput;
+import com.example.stepwise_back.domain.users.service.dto.output.*;
+
+public class UserServiceImpl implements UserService{
+    @Override
+    public UserRegisterOutput register(UserRegisterInput userRegisterInput) {
+        return null;
+    }
+
+    @Override
+    public UserPasswordUpdateOutput updatePassword(UserNicknameUpdateInput userNicknameUpdateInput) {
+        return null;
+    }
+
+    @Override
+    public UserNicknameUpdateOutput updateNickname(UserNicknameUpdateInput userNicknameUpdateInput) {
+        return null;
+    }
+
+    @Override
+    public UserDeleteOutput deleteUser(UserDeleteInput userDeleteInput) {
+        return null;
+    }
+
+    @Override
+    public UserLoginOutput login(UserLoginInput userLoginInput) {
+        return null;
+    }
+
+    @Override
+    public boolean validateNicknameUniqueness(String nickname) {
+        return false;
+    }
+
+    @Override
+    public boolean validateUserIdUniqueness(String userId) {
+        return false;
+    }
+}
