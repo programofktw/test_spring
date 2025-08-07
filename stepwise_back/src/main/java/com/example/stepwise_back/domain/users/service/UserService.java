e57c82c724cbd58ac0399582c96d47f1cbca0408
programofktw <thkim000317@naver.com>
2025-08-07 16:59:50 +0900

    [FEAT] : 유저 닉네임 변경 관련 input output 제작

--- stepwise_back/src/main/java/com/example/stepwise_back/domain/users/service/UserService.java
+++ stepwise_back/src/main/java/com/example/stepwise_back/domain/users/service/UserService.java
@@ -1,6 +1,8 @@
 package com.example.stepwise_back.domain.users.service;
 
+import com.example.stepwise_back.domain.users.service.dto.input.UserDeleteInput;
 import com.example.stepwise_back.domain.users.service.dto.input.UserLoginInput;
+import com.example.stepwise_back.domain.users.service.dto.output.UserDeleteOutput;
 import com.example.stepwise_back.domain.users.service.dto.output.UserLoginOutput;
 
 public interface UserService {
@@ -11,7 +13,7 @@ public interface UserService {
 
     public void updateNickname();
 
-    public void deleteUser();
+    public UserDeleteOutput deleteUser(UserDeleteInput userDeleteInput);
 
     public UserLoginOutput login(UserLoginInput userLoginInput);
 }
