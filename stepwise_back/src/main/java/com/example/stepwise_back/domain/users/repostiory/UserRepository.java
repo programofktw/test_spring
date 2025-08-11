05461ea8116861faed7c25436709be4c2cdf7352
programofktw <thkim000317@naver.com>
2025-08-11 15:05:08 +0900

    [STYLE] 패키지 명 오타 수정(uesrs -> users)

--- stepwise_back/src/main/java/com/example/stepwise_back/domain/users/repostiory/UserRepository.java
+++ stepwise_back/src/main/java/com/example/stepwise_back/domain/users/repostiory/UserRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
     Boolean existsUsersByUserId(String userID);
-
 }
